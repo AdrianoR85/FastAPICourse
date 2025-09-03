cf0be246395a03220cc4151c5f2fa55e5d49f287
adrianor85 <adrianorosa085@gmail.com>
2025-09-03 08:31:01 -0300

    Query Paramaters Part 2

--- project_1/books.py
+++ project_1/books.py
@@ -24,6 +24,7 @@ async def read_book_by_title(book_title: str):
     
   return {"Failed": "Title not Found"}
 
+
 @app.get("/books/")
 async def read_category_query(book_category: str):
   book_list = []
@@ -41,3 +42,23 @@ async def read_category_query(book_category: str):
     return book_list
   else:
     return {"Not Found": "No book were found." }    
+
+@app.get("/books/{book_author}/")
+async def read_author_category_by_query(book_author: str, book_category: str) -> list | dict:
+  book_list = []
+
+  for book in BOOKS:
+    author = book.get("author")
+    category = book.get("category")
+
+    if not author and not book_category:
+      return {"Failed": "author or category is required"}
+    
+    if author.casefold() == book_author and category.casefold() == book_category: # type: ignore
+      book_list.append(book)
+  
+  if book_list:
+    return book_list
+  else:
+    return {"Not Found": "No book were found." }  
+  

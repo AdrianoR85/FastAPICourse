ec87eb43d6ead4b183766dcb0b0a9072899af3d0
adrianor85 <adrianorosa085@gmail.com>
2025-09-08 18:56:40 -0300

    Book Request

--- book_project/book.py
+++ book_project/book.py
@@ -1,3 +1,6 @@
+from fastapi import FastAPI, Body
+from pydantic import BaseModel
+
 class Book:
   id: int
   title: str
@@ -11,4 +14,69 @@ class Book:
     self.author = author
     self.description = description
     self.rating = rating
-  
+
+
+class BookRequest(BaseModel):
+  id: int
+  title: str
+  author: str
+  description: str
+  rating: int
+
+
+BOOKS = [
+    Book(
+        1,
+        "To Kill a Mockingbird",
+        "Harper Lee",
+        "A novel about the serious issues of rape and racial inequality told through the eyes of a child.",
+        5
+    ),
+    Book(
+        2,
+        "1984",
+        "George Orwell",
+        "A dystopian novel that explores government surveillance and totalitarianism.",
+        5
+    ),
+    Book(
+        3,
+        "The Great Gatsby",
+        "F. Scott Fitzgerald",
+        "A story of the mysterious millionaire Jay Gatsby and his passion for the beautiful Daisy Buchanan.",
+        4
+    ),
+    Book(
+        4,
+        "Pride and Prejudice",
+        "Jane Austen",
+        "A romantic novel that also critiques the British landed gentry of the early 19th century.",
+        5
+    ),
+    Book(
+        5,
+        "Moby-Dick",
+        "Herman Melville",
+        "The quest of Ishmael and Captain Ahab for the elusive white whale, Moby Dick.",
+        4
+    ),
+    Book(
+        6,
+        "The Catcher in the Rye",
+        "J.D. Salinger",
+        "A story about teenage rebellion and alienation through the eyes of Holden Caulfield.",
+        4
+    )
+]
+
+
+app = FastAPI()
+
+@app.get("/books")
+async def read_all_books():
+  return BOOKS
+
+@app.post("/create-book")
+async def create_book(book_request: BookRequest):
+  new_book = Book(**book_request.model_dump())
+  BOOKS.append(new_book)
--- book_project/routes.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-from data import BOOKS
-app = FastAPI()
-
-@app.get("/books")
-async def read_all_books():
-  return BOOKS

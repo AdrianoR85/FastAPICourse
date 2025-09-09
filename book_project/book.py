bb95581d6348fc30f0d5ebe98e4320f903b15410
adrianor85 <adrianorosa085@gmail.com>
2025-09-09 16:47:50 -0300

    data validation

--- book_project/book.py
+++ book_project/book.py
@@ -1,10 +1,11 @@
+from typing import Optional
 from fastapi import FastAPI, Body
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class Book:
   id: int
-  title: str
-  author: str
+  title: str 
+  author: str 
   description: str
   rating: int
 
@@ -17,11 +18,22 @@ class Book:
 
 
 class BookRequest(BaseModel):
-  id: int
-  title: str
-  author: str
-  description: str
-  rating: int
+  id: Optional[int] = Field(description="ID is not needed on create", default=None)
+  title: str = Field(min_length=3)
+  author: str = Field(min_length=3)
+  description: str = Field(min_length=1, max_length=200)
+  rating: int = Field(gt=0, lt=6)
+
+  model_config = {
+    "json_schema_extra": {
+      "example": {
+        "title": "A new book",
+        "author": "adriano",
+        "description": "A new description of a book",
+        "rating": 5
+      }
+    }
+  }
 
 
 BOOKS = [
@@ -76,7 +88,18 @@ app = FastAPI()
 async def read_all_books():
   return BOOKS
 
+@app.get("/books/{book_id}")
+async def read_book(book_id: int):
+  for book in BOOKS:
+    if book_id == book.id:
+      return book
+
 @app.post("/create-book")
 async def create_book(book_request: BookRequest):
   new_book = Book(**book_request.model_dump())
-  BOOKS.append(new_book)
+  BOOKS.append(find_book_id(new_book))
+
+
+def find_book_id(book:Book):
+  book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id + 1
+  return book

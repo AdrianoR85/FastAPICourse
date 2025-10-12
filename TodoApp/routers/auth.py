3c4a663a0187301ece88e9473b162709c1db237a
adrianor85 <adrianorosa085@gmail.com>
2025-10-12 16:28:41 -0300

    decode

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -7,16 +7,21 @@ from sqlalchemy.orm import Session
 from database import SessionLocal
 from models import Users
 from passlib.context import CryptContext
-from fastapi.security import OAuth2PasswordRequestForm
-from jose import jwt
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from jose import jwt, JWTError
 
-router = APIRouter()
+router = APIRouter(
+  prefix='/auth',
+  tags=['auth']
+)
 
 SECRET_KEY = "812a56af29e9b761ede2838870f165da1ddb7d36c8559195141009e770b1be2b"
 ALGORITH = "HS256"
 
 
 pwd_context = CryptContext(schemes=['argon2'], deprecated='auto')
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl='auth/token')
+
 
 class CreateUserRequest(BaseModel):
   username: str
@@ -60,7 +65,21 @@ def craete_access_token(username:str, user_id: int, expires_date: timedelta):
   return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITH)
 
 
-@router.post("/auth", status_code=status.HTTP_201_CREATED)
+async def get_current_token(token: Annotated[str, Depends(oauth2_bearer)]):
+  try:
+    payload = jwt.decode(token,SECRET_KEY, algorithms=ALGORITH)
+    username: str = payload.get('sub') # type: ignore
+    user_id: int = payload.get('id') # type: ignore
+    if username is None and user_id is None:
+      raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, 
+                          detail="Could not validation user")
+    
+    return {'username': username, 'id': user_id}
+  except JWTError:
+    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, 
+                          detail="Could not validation user")
+
+@router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_user(db: db_dependency, 
                       create_user_request: CreateUserRequest):
   
@@ -84,7 +103,8 @@ async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm,
   
   user = authenticate_user(form_data.username, form_data.password, db)
   if not user:
-    return "Failed Authentication"
+    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, 
+                          detail="Could not validation user")
   
   token = craete_access_token(user.username, user.id, timedelta(minutes=20)) # type: ignore
   return {'access_token': token, 'token_type': 'bearer'}  

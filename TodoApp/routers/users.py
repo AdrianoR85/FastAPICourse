51f82e07c23477f1705dd679b1de33fb15a92c59
adrianor85 <adrianorosa085@gmail.com>
2025-11-09 20:55:44 -0200

    Add a phone number field as required when we create a new user.

--- TodoApp/routers/users.py
+++ TodoApp/routers/users.py
@@ -61,3 +61,13 @@ async def change_password(
   db.commit()
 
   return {"message": "Password updated successfully"}
+
+@router.put("/phonenumber/{phone_number}", status_code=status.HTTP_204_NO_CONTENT)
+async def change_phone_number(user: user_dependecy, db: db_dependency, phone_number: str):
+  if user is None:
+    raise HTTPException(status_code=401, detail="Authentication Failed")
+  
+  user_model = db.query(Users).filter(Users.id == user.get("id")).first()
+  user_model.phone_number = phone_number # type: ignore
+  db.add(user_model)
+  db.commit()

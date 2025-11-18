95a1c180712c9c100035e34f527e16fabe704403
adrianor85 <adrianorosa085@gmail.com>
2025-11-18 15:02:52 -0300

    Installed need libs

--- TodoApp/routers/todos.py
+++ TodoApp/routers/todos.py
@@ -7,7 +7,10 @@ from starlette import status
 from ..models import Todos
 from .auth import get_current_user
 
-router = APIRouter()
+router = APIRouter(
+  prefix='/todos',
+  tags=['todos']
+)
 
 """Dependency that manages DB sessions."""
 def get_db():

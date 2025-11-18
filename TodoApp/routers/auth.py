06dd41836b9619e6b980e2cae90ca7774e6accf5
adrianor85 <adrianorosa085@gmail.com>
2025-11-18 16:18:33 -0300

    Frontend - Login page

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -1,14 +1,15 @@
 from datetime import datetime, timedelta, timezone
 from typing import Annotated
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Request
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from fastapi.templating import Jinja2Templates
 from starlette import status
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
-from ..database import SessionLocal
-from ..models import Users
 from passlib.context import CryptContext
-from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from jose import jwt, JWTError
+from ..models import Users
+from ..database import SessionLocal
 
 router = APIRouter(
   prefix='/auth',
@@ -47,7 +48,15 @@ def get_db():
 
 db_dependency = Annotated[Session, Depends(get_db)]
 
+templates = Jinja2Templates(directory="TodoApp/templates")
 
+### Pages ###
+@router.get("/login-page")
+def render_login_page(request: Request):
+  return templates.TemplateResponse("login.html", {"request": request})
+
+
+### Endpoints ###
 def authenticate_user(username: str, password: str, db):
   user = db.query(Users).filter(Users.username == username).first()
   if not user:
